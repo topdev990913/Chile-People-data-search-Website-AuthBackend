07a7f3d502e5b89754e9345fe157e628fd3a60c1
TopDevelpoer <133781576+PETER990913@users.noreply.github.com>
2023-07-10 16:28:02 -0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/mydatabase"
+    // url: "mongodb://localhost:27017/mydatabase"
+    url: "mongodb+srv://jeremyt1132:AlBsrejO9uAgtJIM@freedb.yul9lsv.mongodb.net/mydatabase"
   };
